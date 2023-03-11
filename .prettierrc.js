faf4745c817f00fc2a7669a65852c25b046bf385
Lorenz Nimmervoll <admin@nimmervoll.work>
2023-03-11 11:07:16 +0100

    chore: use @atws/projen-config, upgrade projen

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require('@atws/prettier-config')
+}
--- .projenrc.js
+++ .projenrc.js
@@ -1,33 +1,53 @@
-const { awscdk } = require("projen");
+const {
+  PrettierConfig,
+  EslintConfig,
+  VscodeConfig,
+} = require('@atws/projen-config')
+const { awscdk } = require('projen')
 
-const cdkVersion = "2.51.0";
+const cdkVersion = '2.51.0'
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Lorenz Nimmervoll",
-  authorAddress: "admin@nimmervoll.work",
+  author: 'Lorenz Nimmervoll',
+  authorAddress: 'admin@nimmervoll.work',
   cdkVersion,
-  defaultReleaseBranch: "main",
-  name: "cdk-self-destruct",
-  repositoryUrl: "https://github.com/NimmLor/cdk-self-destruct.git",
-  stability: "experimental",
+  defaultReleaseBranch: 'main',
+  name: 'cdk-self-destruct',
+  repositoryUrl: 'https://github.com/NimmLor/cdk-self-destruct.git',
+  stability: 'experimental',
   description:
-    "A construct that allows you to self-destruct your AWS resources in a given stack",
-  keywords: ["cdk", "awscdk", "aws-cdk"],
+    'A construct that allows you to self-destruct your AWS resources in a given stack',
+  keywords: ['cdk', 'awscdk', 'aws-cdk'],
   majorVersion: 1,
 
   deps: [],
-  devDeps: ["aws-sdk", "esbuild"],
-  packageName: "cdk-self-destruct",
+  devDeps: ['aws-sdk', 'esbuild', '@atws/projen-config'],
+  packageName: 'cdk-self-destruct',
   jest: true,
   jestOptions: {
     extraCliOptions: [
       '--testMatch "**/(test|src)/**/*(*.)@(spec|test).ts?(x)"',
     ],
   },
-  gitignore: ["cdk.out"],
+  gitignore: ['cdk.out'],
   eslintOptions: {
     prettier: true,
   },
   prettier: true,
-});
-project.synth();
+})
+
+new PrettierConfig(project)
+
+new EslintConfig(project, {
+  cdkFileRegex: 'src/**/*.ts',
+})
+
+new VscodeConfig(project, {
+  vscodeExtensions: {
+    addCdkExtensions: true,
+    addCoreExtensions: true,
+    addNodeExtensions: true,
+  },
+})
+
+project.synth()
